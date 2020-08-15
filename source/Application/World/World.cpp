f3584995ff912a1b0682b43605d2f66178aeba7f
gnog <4466113@gmail.com>
2020-08-16 00:37:12 +0500

    got rid of other useless stuff again (due to stupid git sorry)

--- source/Application/World/World.cpp
+++ source/Application/World/World.cpp
@@ -4,8 +4,7 @@ World::World() {
     glEnable(GL_DEPTH_TEST);
     
     shader.loadFromFile("Shaders/m.vertex.glsl", "Shaders/m.fragment.glsl");
-    lightBoxShader.loadFromFile("Shaders/lightbox.vertex.glsl", "Shaders/lightbox.fragment.glsl");
-    skyBox.emplace("Shaders/skybox.vertex.glsl", "Shaders/skybox.fragment.glsl", std::vector<std::string>{
+    skyBox = std::make_unique<SkyBox>("Shaders/skybox.vertex.glsl", "Shaders/skybox.fragment.glsl", std::vector<std::string>{
             "SkyBoxTextures/right.bmp",
             "SkyBoxTextures/left.bmp",
             "SkyBoxTextures/top.bmp",
@@ -13,41 +12,8 @@ World::World() {
             "SkyBoxTextures/front.bmp",
             "SkyBoxTextures/back.bmp"
     });
-    OpenGLModel model;
-    model.addCollider(
-            glm::vec3(-1.0, 1.0, 1.0),
-            glm::vec3(1.0, 1.0, 1.0),
-            glm::vec3(1.0, -1.0, 1.0),
-            glm::vec3(-1.0, -1.0, 1.0),
-            glm::vec3(-1.0, 1.0, -1.0),
-            glm::vec3(1.0, 1.0, -1.0),
-            glm::vec3(1.0, -1.0, -1.0),
-            glm::vec3(-1.0, -1.0, -1.0),
-            glm::vec3(1.0, 1.0, 1.0),
-            glm::vec3(0, 0, 1.0),
-            glm::vec3(0, 0, -1.0),
-            glm::vec3(-1.0, 0, 0),
-            glm::vec3(1.0, 0, 0),
-            glm::vec3(0, 1.0, 0),
-            glm::vec3(0, -1.0, 0)
-    );
-    glGenVertexArrays(1, &vaoid);
-    glBindVertexArray(vaoid);
-    modelVBO.create(GL_ARRAY_BUFFER, GL_STATIC_DRAW, model.size());
-    modelVBO.bind();
-    modelVBO.fill(0, model.link());
-    glEnableVertexAttribArray(0);
-    glEnableVertexAttribArray(1);
-    glEnableVertexAttribArray(2);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 9 * sizeof(float), (void*) nullptr);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 9 * sizeof(float), (void*) (3 * sizeof(float)));
-    glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 9 * sizeof(float), (void*) (6 * sizeof(float)));
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindVertexArray(0);
 }
-World::~World() {
-    glDeleteVertexArrays(1, &vaoid);
-}
+World::~World() = default;
 void World::draw(Player& player) {
     glm::mat4 view = player.getViewMatrix();
     glm::mat4 projection = player.getProjectionMatrix();
@@ -55,8 +21,5 @@ void World::draw(Player& player) {
     skyBox->draw(projection * glm::mat4(glm::mat3(view))); // TODO: need to be optimized()()
     sf::Shader::bind(&shader);
     shader.setUniform("VP", sf::Glsl::Mat4(&vp[0][0]));
-    glBindVertexArray(vaoid);
-    glDrawArrays(GL_TRIANGLES, 0, 6000);
-    glBindVertexArray(0);
     sf::Shader::bind(nullptr);
 }
--- source/Application/World/World.hpp
+++ source/Application/World/World.hpp
@@ -6,16 +6,12 @@
 
 #include "../ogl.hpp"
 #include "../Player/Player.hpp"
-#include "OpenGLWrappers/VBO/VBO.hpp"
 #include "SkyBox/SkyBox.hpp"
 #include "OpenGLWrappers/OpenGLModel/OpenGLModel.hpp"
 class World {
     private:
         sf::Shader shader;
-        sf::Shader lightBoxShader;
-        uint32_t vaoid = 0;
-        VBO<glm::vec3> modelVBO;
-        std::optional<SkyBox> skyBox;
+        std::unique_ptr<SkyBox> skyBox;
     public:
         World();
         ~World();
