4110b708076365bfe86e93ec11ecd21ae6f4d7aa
gnog <4466113@gmail.com>
2020-08-17 06:09:42 +0500

    vector 3 less functor implementation

--- source/Application/Vector3LessThan/Vector3LessThan.cpp
+++ source/Application/Vector3LessThan/Vector3LessThan.cpp
@@ -2,5 +2,9 @@
 
 template<class T>
 bool Vector3LessThan<T>::operator()(const sf::Vector3<T>& v1, const sf::Vector3<T>& v2) const {
-    return false;
+    if (v1.x < v2.x) return true;
+    if (v1.x > v2.x) return false;
+    if (v1.y < v2.y) return true;
+    if (v1.y > v2.y) return false;
+    return v1.z < v2.z;
 }
